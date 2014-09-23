7d7e16f02610258e067af13c80ea3b7483533807
Gabriel Martinez <reitaka@gmail.com>
2014-09-23 09:32:18 -0700

    Initial working version of Conway's Game of Life

---
+++ src/grid.rs
@@ -0,0 +1,112 @@
+use std::collections::Bitv;
+use std::rand;
+
+#[deriving(Clone)]
+pub struct BoolGrid {
+    pub width: uint,
+    pub height: uint,
+    pub grid: Vec<Bitv>,
+}
+
+struct Cell {
+    x: uint,
+    y: uint,
+    alive: bool,
+}
+
+pub struct LifeBoard {
+    pub grid: BoolGrid,
+    old_grid: BoolGrid,
+}
+
+impl BoolGrid {
+    pub fn new(width: uint, height: uint) -> BoolGrid {
+        let mut grid = Vec::with_capacity(height);
+        for _ in range(0, height) {
+            grid.push(Bitv::with_capacity(width, false));
+        }
+
+        BoolGrid {
+            width: width,
+            height: height,
+            grid: grid,
+        }
+    }
+
+    pub fn check_point(&self, x: uint, y: uint) -> bool {
+        x < self.width && y < self.height
+    }
+
+    pub fn get_neighbors(&self, x: uint, y: uint) -> Vec<Cell> {
+        let mut neighbors = vec![];
+        for j in range(y - 1, y + 2) {
+            for i in range(x - 1, x + 2) {
+                if (x != i || y != j) && self.check_point(i, j) {
+                    neighbors.push(Cell {
+                        x: i,
+                        y: j,
+                        alive: self.grid[j][i],
+                    });
+                }
+            }
+        }
+        neighbors
+    }
+
+    pub fn get(&self, x: uint, y: uint) -> bool {
+        self.grid[y][x]
+    }
+
+    pub fn set(&mut self, x: uint, y: uint, value: bool) {
+        self.grid.get_mut(y).set(x, value);
+    }
+
+    pub fn clear(&mut self) {
+        for bv in self.grid.iter_mut() {
+            bv.clear();
+        }
+    }
+
+    pub fn randomize(&mut self) {
+        for bv in self.grid.iter_mut() {
+            for i in range(0, bv.len()) {
+                bv.set(i, rand::random());
+            }
+        }
+    }
+}
+
+impl LifeBoard {
+    pub fn new(width: uint, height: uint) -> LifeBoard {
+        LifeBoard {
+            grid: BoolGrid::new(width, height),
+            old_grid: BoolGrid::new(width, height),
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.grid.clear();
+    }
+
+    pub fn randomize(&mut self) {
+        self.grid.randomize();
+    }
+
+    pub fn step(&mut self) {
+        self.old_grid = self.grid.clone();
+
+        for j in range(0, self.grid.height) {
+            for i in range(0, self.grid.width) {
+                let neighbor_count = self.old_grid.get_neighbors(i, j).iter()
+                    .filter(|&cell| cell.alive)
+                    .count();
+                if self.grid.get(i, j) {
+                    self.grid.set(
+                        i, j, neighbor_count >= 2 && neighbor_count <= 3);
+                } else {
+                    self.grid.set(i, j, neighbor_count == 3);
+                }
+            }
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,123 @@
+extern crate sdl2;
+
+use sdl2::event;
+use sdl2::keycode;
+use sdl2::pixels;
+use sdl2::render::{
+    Accelerated,
+    //BlendBlend,
+    DriverAuto,
+    Renderer,
+};
+use sdl2::timer;
+use sdl2::video::{
+    PosUndefined,
+    Shown,
+    Window,
+};
+
+mod grid;
+
+static SCREEN_WIDTH: uint = 500;
+static SCREEN_HEIGHT: uint = 500;
+
+static FPS: uint = 60;
+
+static BLACK: pixels::Color = pixels::RGB(0, 0, 0);
+static WHITE: pixels::Color = pixels::RGB(255, 255, 255);
+
+fn draw_board(renderer: &Renderer<Window>, board: &grid::LifeBoard) {
+    let mut cell_rect = sdl2::rect::Rect {
+        x: 0,
+        y: 0,
+        w: (SCREEN_WIDTH/board.grid.width) as i32,
+        h: (SCREEN_HEIGHT/board.grid.height) as i32,
+    };
+
+    renderer.set_draw_color(WHITE);
+    for (j, bv) in board.grid.grid.iter().enumerate() {
+        for (i, alive) in bv.iter().enumerate() {
+            if alive {
+                cell_rect.x = i as i32 * cell_rect.w;
+                cell_rect.y = j as i32 * cell_rect.h;
+
+                renderer.fill_rect(&cell_rect);
+            }
+        }
+    }
+}
+
+fn run(step_time_ms: uint, width: uint, height: uint) {
+    let mut board = grid::LifeBoard::new(width, height);
+
+    let window = Window::new("Life",
+                             PosUndefined,
+                             PosUndefined,
+                             SCREEN_WIDTH as int,
+                             SCREEN_HEIGHT as int,
+                             Shown).unwrap();
+    let renderer = Renderer::from_window(
+        window, DriverAuto, Accelerated).unwrap();
+    //renderer.set_blend_mode(BlendBlend);
+
+    sdl2::mouse::show_cursor(true);
+
+    let mut running = true;
+    let mut simulate = false;
+    let mut last_step_time = timer::get_ticks();
+
+    while running {
+        let start_time = timer::get_ticks();
+
+        loop {
+            match event::poll_event() {
+                event::KeyDownEvent(_, _, key, _, _) => {
+                    println!("{}", key);
+                    match key {
+                        keycode::SpaceKey => simulate = !simulate,
+                        keycode::RKey => board.randomize(),
+                        keycode::CKey => board.clear(),
+                        _ => continue,
+                    }
+                }
+                event::QuitEvent(..) => running = false,
+                event::NoEvent => break,
+                _ => continue,
+            }
+        }
+
+        let current_time = timer::get_ticks();
+        if simulate && (current_time - last_step_time >= step_time_ms) {
+            board.step();
+            last_step_time = timer::get_ticks();
+        }
+
+        // Clear the screen
+        renderer.set_draw_color(BLACK);
+        renderer.clear();
+
+        // Draw
+        draw_board(&renderer, &board);
+
+        // Update buffers
+        renderer.present();
+
+        let ms_per_frame = 1000 / FPS;
+        let elapsed_time = timer::get_ticks() - start_time;
+        if elapsed_time < ms_per_frame {
+            timer::delay(ms_per_frame - elapsed_time);
+        }
+    }
+}
+
+fn main() {
+    let step_time = 200;
+    let width = 50;
+    let height = 50;
+
+    sdl2::init(sdl2::InitVideo | sdl2::InitTimer);
+
+    run(step_time, width, height);
+
+    sdl2::quit();
+}
